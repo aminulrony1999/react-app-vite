ccab74be547ec58d8aa1054ab990a5c6be843ce2
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-01 14:05:13 +0600

    data corected

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div>
-      <Device name = "Lenov Flex IV core i7 10th gen" price="130K BDT"></Device>
+      <Device name = "Lenov Flex 5 core i7 10th gen" price="130K BDT"></Device>
       <Device name = "Samsung Galaxy M21" price = "20K BDT"></Device>
     </div>
   )
