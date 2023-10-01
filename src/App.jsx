497164a45977181b277b95b4005dea4d0379404a
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-01 14:04:32 +0600

    nyz

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div>
       <Device name = "Lenov Flex IV core i7 10th gen" price="130K BDT"></Device>
+      <Device name = "Samsung Galaxy M21" price = "20K BDT"></Device>
     </div>
   )
 }
