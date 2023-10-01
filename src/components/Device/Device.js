f8090c8bac9ebd8910cd1252455d55a3ad2fe0cb
aminulrony1999 <aminulrony1999@gmail.com>
2023-10-01 13:47:30 +0600

    nnnn

---
+++ src/components/Device/Device.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Device.css';
+
+const Device = () => {
+    return (
+        <div>
+            <h3>I have a Device</h3>
+        </div>
+    );
+};
+
+export default Device;
